85cc202802469b0acb6817c6a2e07d27f37c51da
Sedat yıldız <ikinciyargic@gmail.com>
2025-12-28 14:42:58 +0300

    Initial Commit

---
+++ StepCounterWidget/StepCounterWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepCounterWidgetApp.swift
+//  StepCounterWidget
+//
+//  Created by Sedat Yıldız on 28.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct StepCounterWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StepCounterWidgetTests/StepCounterWidgetTests.swift
@@ -0,0 +1,17 @@
+//
+//  StepCounterWidgetTests.swift
+//  StepCounterWidgetTests
+//
+//  Created by Sedat Yıldız on 28.12.2025.
+//
+
+import Testing
+@testable import StepCounterWidget
+
+struct StepCounterWidgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
